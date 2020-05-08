0b40465a487db2b11e8894681497543abb21997a
MikailaAkeredolu <makerofappsdotcom@gmail.com>
2020-05-08 09:23:15 -0400

    app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
-<<<<<<< HEAD
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-=======
 import { RecipeDetailsComponent } from './recipes/recipe-details/recipe-details.component';
->>>>>>> recipe-details
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     RecipesComponent,
-<<<<<<< HEAD
-    RecipeListComponent
-=======
+    RecipeListComponent,
     RecipeDetailsComponent
->>>>>>> recipe-details
   ],
   imports: [
     BrowserModule,
